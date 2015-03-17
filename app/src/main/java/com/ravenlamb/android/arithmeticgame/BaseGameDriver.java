c31846832b5abad10b89191e42e3624eb08ced80
Karven Lam <karven.lam@nyu.edu>
2015-03-17 01:33:14 -0400

    Removed Setting from menu.

--- app/src/main/java/com/ravenlamb/android/arithmeticgame/BaseGameDriver.java
+++ app/src/main/java/com/ravenlamb/android/arithmeticgame/BaseGameDriver.java
@@ -73,6 +73,20 @@ public class BaseGameDriver {
                 cells[i][j]=random.nextInt(10);
             }
         }
+        //for screenshots
+//        cells[0][3]=6;
+//        cells[1][4]=5;
+//        cells[2][5]=4;
+//        cells[2][0]=5;
+//        cells[2][1]=3;
+//        cells[2][2]=0;
+//        cells[2][3]=1;
+//        cells[3][4]=9;
+//        cells[4][4]=7;
+//        cells[5][4]=5;
+//        cells[3][1]=1;
+//        cells[4][1]=2;
+//        cells[5][1]=3;
     }
 
     public static boolean isValidOperator(int operator){
